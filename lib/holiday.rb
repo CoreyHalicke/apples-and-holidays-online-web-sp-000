f83406ea260c07cf4144bd979e5648659af373bc
Corey H <c.l.halicke@gmail.com>
2019-04-29 01:24:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.each do |holi, supplies|
       supplies.each do |supply|
         if supply == "BBQ"
-          puts holi
+          return holi
         end
       end
     end
